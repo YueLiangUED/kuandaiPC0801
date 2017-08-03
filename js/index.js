eb6be4526c3e31d8b743f9047519dfa05ba2ac6b
ChengYien <cye_orange@163.com>
2017-08-03 15:56:52 +0800

    change style

--- js/index.js
+++ js/index.js
@@ -11,11 +11,29 @@ $(document).ready(function(){
         var $formGroup = $(this).parents('.form-group');
         var $selectSpan = $formGroup.find('.select-span');
         var thisVal = $(this).val();
-        console.log(thisVal);
         $selectSpan.html(thisVal);
     });
     $('.form-group').on('click', function () {
         $('.form-group').removeClass('act');
+        $(this).addClass('act');
+    });
+    $('.router-side ul li').on('click', function () {
+        var thisIndex = $(this).index();
+        $('.router-item').hide();
+        var thisItem = $('.router-item')[thisIndex];
+        $(thisItem).show();
+
+        $('.router-side ul li').removeClass('act');
+        $(this).addClass('act');
+        $(window).scrollTop(0);
+    });
+    $('.kd-btn-group-tab .kd-btn').on('click', function () {
+        var thisIndex = $(this).index();
+        $('.tab-img').hide();
+        var thisItem = $('.tab-img')[thisIndex];
+        $(thisItem).show();
+
+        $('.kd-btn-group-tab .kd-btn').removeClass('act');
         $(this).addClass('act');
     });
 });
