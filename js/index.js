2e478ec3d643b1e24177ae6d5d13eccb97cd03eb
ChengYien <cye_orange@163.com>
2017-08-03 13:15:10 +0800

    add file

--- js/index.js
+++ js/index.js
@@ -2,9 +2,20 @@
 * @Author: Marte
 * @Date:   2017-05-26 11:03:33
 * @Last Modified by:   Marte
-* @Last Modified time: 2017-05-26 11:03:58
+* @Last Modified time: 2017-08-02 16:54:44
 */
 
 $(document).ready(function(){
-    
+    // select功能
+    $('select').on('change', function () {
+        var $formGroup = $(this).parents('.form-group');
+        var $selectSpan = $formGroup.find('.select-span');
+        var thisVal = $(this).val();
+        console.log(thisVal);
+        $selectSpan.html(thisVal);
+    });
+    $('.form-group').on('click', function () {
+        $('.form-group').removeClass('act');
+        $(this).addClass('act');
+    });
 });
